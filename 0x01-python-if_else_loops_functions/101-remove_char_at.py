a6090ac67d377c2df3de238d1410920e3b8bf993
maqamylee0 <maqamylee0@gmail.com>
2023-05-09 02:07:04 -0700

    str_copy

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    i = 0
+    str_copy = ""
+    for char in str:
+        if i == n:
+            continue
+            i++
+        else
+            str_copy += char
+            i++
+    return str_copy
