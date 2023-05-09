12c31f6668b46b60efcdfff9b9bd9c9961f852df
maqamylee0 <maqamylee0@gmail.com>
2023-05-09 16:34:04 -0700

    numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 10:
-        print("0{}".format(i), sep=", ", end=" ")
+        print("0{}".format(i), end=", ")
     else:
-        print("{}".format(i), sep=", ", end=" ")
+        print("{}".format(i), end=", ")
