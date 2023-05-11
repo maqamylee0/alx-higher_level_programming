30848855df30163299b99f07bb64688b0a920878
maqamylee0 <maqamylee0@gmail.com>
2023-05-11 01:36:55 -0700

    calc

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,5 +2,6 @@
 from add_0 import add
 a = 1
 b = 2
-result = add(1, 2)
-print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    result = add(1, 2)
+    print("{} + {} = {}".format(a, b, result))
