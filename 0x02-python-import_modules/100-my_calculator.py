b01bb4ead6c501ce490dc70d2e1bee1b352104cf
maqamylee0 <maqamylee0@gmail.com>
2023-05-11 05:13:23 -0700

    operator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,16 +12,16 @@ if __name__ == "__main__":
     else:
         if argv[2] == '+':
             result = add(int(argv[1]), int(argv[3]))
-            print("{} {} {} {}".format(argv[1], argv[2], argv[3], result))
+            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], result))
         elif argv[2] == '-':
             result = sub(int(argv[1]), int(argv[3]))
-            print("{} {} {} {}".format(argv[1], argv[2], argv[3], result))
+            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], result))
         elif argv[2] == '*':
             result = mul(int(argv[1]), int(argv[3]))
-            print("{} {} {} {}".format(argv[1], argv[2], argv[3], result))
+            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], result))
         elif argv[2] == '/':
             result = div(int(argv[1]), int(argv[3]))
-            print("{} {} {} {}".format(argv[1], argv[2], argv[3], result))
+            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], result))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
