dd21f3121dbb373b434adce5964a693fa0d43608
maqamylee0 <maqamylee0@gmail.com>
2023-05-11 03:24:40 -0700

    index

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,11 @@ if __name__ == "__main__":
     length = len(argv)
 
     if length == 2:
-        print("{} argument".format(length - 1))
-    else:
+        print("{} argument:".format(length - 1))
+    elif length == 1:
         print("{} arguments".format(length - 1))
+    else:
+        print("{} arguments:".format(length - 1))
 
     for i in range(length - 1):
         print("{}: {}".format(i + 1, argv[i + 1]))
