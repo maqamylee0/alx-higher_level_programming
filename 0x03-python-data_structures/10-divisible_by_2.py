b4a14484d9056942738bbe5e9df073f56f3b04d8
maqamylee0 <maqamylee0@gmail.com>
2023-05-14 05:44:08 -0700

    div

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,8 +3,8 @@ def divisible_by_2(my_list=[]):
     list2 = []
     for i in my_list:
         if i % 2 == 0:
-            list2[i] = True
+            list2.append(True)
         else:
-            list2[i] = False
+            list2.append(False)
     return list2
 
