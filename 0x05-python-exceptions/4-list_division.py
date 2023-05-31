a4b0bfa4026a2ba147ef18e618377017a567cf4d
maqamylee0 <maqamylee0@gmail.com>
2023-05-31 13:57:01 -0700

    coreections

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,28 +3,16 @@ def list_division(my_list_1, my_list_2, list_length):
     list2 = []
     for i in range(list_length):
         try:
-            if i >= len(my_list_1) or i >= len(my_list_2):
-                raise IndexError("out of range")
-            if my_list_2[i] == 0:
-                raise ZeroDivisionError("division by 0")
-            if not isinstance(my_list_1[i], (int, float)) or
-            not isinstance(my_list_2[i], (int, float)):
-                raise TypeError("wrong type")
             res = my_list_1[i] / my_list_2[i]
-            mod = my_list_1[i] % my_list_2[i]
-            if mod != 0:
-                list2.append(0)
-            else:
-                list2.append(res)
-        except IndexError as e:
-            print(str(e))
-            list2.append(0)
-        except ZeroDivisionError as e:
-            print(str(e))
-            list2.append(0)
-        except TypeError as e:
-            print(str(e))
-            list2.append(0)
+        except IndexError:
+            print("out of range")
+            res = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            res = 0
+        except TypeError:
+            print("wrong type")
+            res = 0
         finally:
-            pass
+            list2.append(res)
     return list2
--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def raise_exception():
-    try:
-        raise TypeError("Exception raised")
-    except TypeError as e:
-        print(str(e))
+    raise TypeError
