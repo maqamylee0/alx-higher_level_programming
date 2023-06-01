b6bb6b92b61a1819040fcf852d39a9dc969ada5c
maqamylee0 <maqamylee0@gmail.com>
2023-05-31 23:10:19 -0700

    public method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""class with 2 methods"""
+
+
+class Square:
+    """square class with 2 methods"""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        seif.__size = size
+
+    def area(self):
+        return self.size ** 2
