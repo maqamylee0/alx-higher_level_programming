b4d0c0bf74c214a6b92784a366130ae67e15164c
maqamylee0 <maqamylee0@gmail.com>
2023-06-09 14:34:05 -0700

    ad test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,7 +4,6 @@
 
 def add_integer(a, b=98):
     """function that adds integers"""
-
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
