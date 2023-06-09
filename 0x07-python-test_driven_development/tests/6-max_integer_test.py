5115085446df289eba290b2f8a2768ef24fa4a03
maqamylee0 <maqamylee0@gmail.com>
2023-06-09 16:06:48 -0700

    fix errors in test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""module tests"""
-
-
+"""Unittest for max_integer([..])
+"""
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
@@ -10,17 +9,17 @@ class TestMaxInteger(unittest.TestCase):
     def test_ints_unorderd(self):
         """tests for integers only list"""
         list1 = [1, 2, 7, 3]
-        self.assertEqual(max_integer(list1, 7))
+        self.assertEqual(max_integer(list1), 7)
 
     def test_repeat(self):
         """checks max with repeated numbers"""
         list2 = [1, 2, 2]
-        self.assertEqual(max_integer(list2, 2))
+        self.assertEqual(max_integer(list2), 2)
 
     def test_ordered(self):
         """checks max in ordered list"""
         list3 = [1, 2, 3]
-        self.assetEqual(max_integer(list3, 3))
+        self.assertEqual(max_integer(list3), 3)
 
 if __name__ == '__main__':
     unittest.main()
