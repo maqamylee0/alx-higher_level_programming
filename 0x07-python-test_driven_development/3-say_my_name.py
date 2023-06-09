52519a96088c4c3502fb06d735ae0551924c8fe2
maqamylee0 <maqamylee0@gmail.com>
2023-06-09 14:18:02 -0700

    my name test

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
+    """function to say my name"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
