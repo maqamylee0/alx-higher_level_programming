7664ccf2fe579fff4806a1dba750579804b41ac3
maqamylee0 <maqamylee0@gmail.com>
2023-06-09 14:57:10 -0700

    4 tests

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr.bin/python3
+"""module that prints a square"""
+
+def print_square(size):
+    """function that prints a square"""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    for i in range(size):
+        print("#" * size)
