d12d025fc31e86ad88839c1017b41613e9d3c6a0
maqamylee0 <maqamylee0@gmail.com>
2023-06-12 13:49:46 -0700

    fix style

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,6 +7,7 @@ class MyInt(int):
     def __eq__(self, other):
         """function that reverses- =="""
         return super().__ne__(other)
+
     def __ne__(self, other):
         """function to reverse !="""
         return super().__eq__(other)
