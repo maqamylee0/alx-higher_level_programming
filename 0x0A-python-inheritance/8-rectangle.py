fb07702fb3125147dab45ec5606956864a63b68d
maqamylee0 <maqamylee0@gmail.com>
2023-06-12 05:52:41 -0700

    fix 7 tests

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,6 +2,8 @@
 """class inheriting form basegeo"""
 
 
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
 class Rectangle(BaseGeometry):
     """class inheriting from base geometry class"""
     def __init__(self, width, height):
