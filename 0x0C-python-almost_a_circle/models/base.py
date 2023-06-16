7092d9b803651ef051193571fc2a44564eb137f1
maqamylee0 <maqamylee0@gmail.com>
2023-06-16 07:54:16 -0700

    base class and setter

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""base class"""
+
+
+class Base:
+    """base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initialising instance"""
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
