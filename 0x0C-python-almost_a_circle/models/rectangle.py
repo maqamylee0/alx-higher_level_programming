38c1eed437945008f01d4822ac3a360f7fd69492
maqamylee0 <maqamylee0@gmail.com>
2023-06-19 13:58:26 -0700

    rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -20,28 +20,97 @@ class Rectangle(Base):
     
     @width.setter
     def width(self, value):
-        self.__width = value
+        """settet  for width"""
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif width <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
+        """gets height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        self.__height = value
+        """sets height value"""
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        elif height <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
 
     @property
     def x(self):
+        """gets x"""
         return self.__x
 
     @x.setter
     def x(self, value):
-        self.__x = x
+        """sets x value"""
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        elif x < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = x
 
     @property
     def y(self):
+        """gets y"""
         return self.__y
 
     @y.setter
     def y(self, value):
-        self.__y = value
+        """sets y value"""
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        elif y < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = value
+
+    def area(self):
+        """calculates area"""
+        return self.__width * self.__height
+
+    def display(self):
+        """displays triangle with # """
+        print("\n" * self.__y, end="")
+        for i in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
+
+    def __str__(self):
+        """display in format [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
+        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - "
+                f"{self.__width}/{self.__height}")
+
+    def update(self, *args, **kwargs):
+        """asigns values with args and kwargs"""
+        if len(args) > 0:
+            self.id = args[0]
+            self.__width = args[1]
+            self.__height = args[2]
+            self.__x = args[3]
+            self.__y = args[4]
+        else:
+            for key, value in kwargs.iteritems():
+                if key == "id":
+                    self.id = value
+                elif key == "width":
+                    self.__width = value
+                elif key == "height":
+                    self.__height = value
+                elif key == "x":
+                    self.__x = value
+                else:
+                    self.__y = value
+
+        def to_dictionary(self):
+            """rectangle dictionary"""
+            return { 'id' : self.id, 'x' : self.x, 'y': self.y,
+                    'width': self.width,'height': self.height}
+
