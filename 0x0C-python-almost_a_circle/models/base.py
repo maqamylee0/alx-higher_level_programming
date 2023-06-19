efbfbd7cf1844818ad633a01b2a0c0e07aff8f86
maqamylee0 <maqamylee0@gmail.com>
2023-06-19 14:29:15 -0700

    base and square

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,3 +13,34 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation to a file"""
+        list_o = []
+        if list_objs is None:
+            list_o = []
+        else:
+            for obj in list_objs:
+                list_o.append(obj.to_dictionary())
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as file:
+            file.write(cls.to_json_string(list_o)
+
+    @staticmethod
+    def from_json_string(json_string):
+       """changes from json to list"""
+       if json_string is None or json_string == "":
+           return []
+       else:
+           return json.loads(json_string)
+
+
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""square module"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """square class basing on rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """initilaise instnce"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """overide str in rectangle"""
+        return (f"[Square] ({self.id}) {self.x}/{self.y} - "
+                f"{self.width}")
+    @property
+    def size(self):
+        """gets size"""
+        return self.height
+
+    @size.setter
+    def size(self, value):
+        """sets value of size"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.width = value
+            self.height = value
+
+    def update(self, *args, **kwargs):
+        """update value"""
+        if len(args) >= 0:
+            self.id = args[0]
+            self.size = args[1]
+            self.x = args[2]
+            self.y = args[3]
+        else:
+            for key, value in kwargs.iteritems():
+                if key == "id":
+                    self.id = value
+                elif key == "size":
+                    self.width = value
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                else:
+                    self.y = value
+
+    def to_dictionary(self):
+        """dictionary rep of square"""
+        return {'id': self.id, 'x': self.x, 'size': self.size,
+                'y': self.y }
+
