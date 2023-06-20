b22ef1ee4a8d64b7f274f5a494a186a7340f3bb5
maqamylee0 <maqamylee0@gmail.com>
2023-06-20 07:05:33 -0700

    fix style

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -100,7 +100,6 @@ class Rectangle(Base):
             self.__x = args[3]
         if len(args) > 4:
             self.__y = args[4]
-            
         else:
             for key, value in kwargs.items():
                 if key == "id":
