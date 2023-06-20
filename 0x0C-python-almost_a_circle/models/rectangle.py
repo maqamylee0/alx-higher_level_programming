89040c3484bbb3ef3d014d9c7d556f593d404284
maqamylee0 <maqamylee0@gmail.com>
2023-06-20 06:48:47 -0700

    fix updates test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,13 +91,17 @@ class Rectangle(Base):
     def update(self, *args, **kwargs):
         """asigns values with args and kwargs"""
         if len(args) > 0:
-            self.id = args[0]
-            self.__width = args[1]
-            self.__height = args[2]
-            self.__x = args[3]
-            self.__y = args[4]
+            self.__width = args[0]
+        if len(args) > 1:
+            self.__height = args[1]
+        if len(args) > 2:
+            self.__x = args[2]
+        if len(args) > 3:
+            self.__y = args[3]
+        if len(args) > 4:
+            self.id = args[4]
         else:
-            for key, value in kwargs.iteritems():
+            for key, value in kwargs.items():
                 if key == "id":
                     self.id = value
                 elif key == "width":
