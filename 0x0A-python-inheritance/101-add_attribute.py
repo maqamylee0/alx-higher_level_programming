75bd0dc7c055ef1efec4cbe8b03c8a11149dfd51
maqamylee0 <maqamylee0@gmail.com>
2023-08-04 10:56:58 -0700

    create a user

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,7 +4,7 @@
 
 def add_attribute(obj, name, value):
     """ adds attribute to obj if possible"""
-    if hasattr(obj, name):
+    if hasattr(type(obj), '__dict__') and name in type(obj).__dict__:
         raise TypeError("can't add new attribute")
     else:
         setattr(obj, name, value)
