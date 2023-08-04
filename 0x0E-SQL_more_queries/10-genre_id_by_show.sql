39a3f050802d64a766ff9c62f26f0a07da79f63d
maqamylee0 <maqamylee0@gmail.com>
2023-08-04 11:08:20 -0700

    list shows

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- lists all shows contained
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres
+JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- lists all cities contained in the database
+
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states
+ON cities.state_id = states.id;
