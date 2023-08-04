f8d7340c401bb05fe7057a09376aae37b5cc1b1f
maqamylee0 <maqamylee0@gmail.com>
2023-08-04 11:03:33 -0700

    create table

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- creates the MySQL server user user_0d_1
+-- user has all privilleges
+-- password set
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
