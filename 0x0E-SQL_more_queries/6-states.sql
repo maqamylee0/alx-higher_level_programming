5fde120dadee8ec6890118c7db5532e39aa3e9cf
maqamylee0 <maqamylee0@gmail.com>
2023-08-04 11:04:18 -0700

    create db

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- creates the database hbtn_0d_usa
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+	id INT AUTO_INCREMENT,
+	name VARCHAR(256),
+	PRIMARY KEY (id)
+);
