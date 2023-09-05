448fc9cfecb9cc369632bc1ea54f305c780821d7
maqamylee0 <maqamylee0@gmail.com>
2023-09-05 10:38:14 +0200

    make semistandard compliant

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 
 function factorial (x) {
-    if (isNaN(x)) {
-      return 1;
-    } else if (x <= 1) {
-      return 1;
-    } else {
-      return x * factorial(x - 1);
-    }
+  if (isNaN(x)) {
+    return 1;
+  } else if (x <= 1) {
+    return 1;
+  } else {
+    return x * factorial(x - 1);
   }
-  
-  const args = process.argv.slice(2);
-  const input = parseInt(args[0]);
-  
-  console.log(factorial(input));
+}
+
+const args = process.argv.slice(2);
+const input = parseInt(args[0]);
+
+console.log(factorial(input));
--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 exports.callMeMoby = function (x, theFunction) {
-    for (let j = 0; j < x; j++) {
-      theFunction();
-    }
-  };
+  for (let j = 0; j < x; j++) {
+    theFunction();
+  }
+};
