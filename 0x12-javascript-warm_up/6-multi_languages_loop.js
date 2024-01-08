786c24f3ff9e25fb3b6c4ac9caa2fa6215de0c53
maqamylee0 <maqamylee0@gmail.com>
2024-01-07 23:10:22 -0800

    print array elements

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+//script to print multiple texts to console using array and loop
+
+const myVar = ["C is fun", "Python is cool", "JavaScript is amazing"]
+
+myVar.forEach((element)=>
+{
+	console.log(element);
+});
