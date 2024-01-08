ca5c47d00d6fc601404492badb5cc66e66e62199
maqamylee0 <maqamylee0@gmail.com>
2024-01-07 23:03:06 -0800

    check if number

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+//convert arg1 to integer
+
+const myVar = parseInt(process.argv[2]);
+
+if (isNaN(myVar))
+{
+	console.log("Not a number");
+}else
+{	
+	console.log("My number:" + myVar);
+}
