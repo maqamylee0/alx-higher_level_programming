38b6d7b61a59b82a1204dd01c7bf1c067912df10
maqamylee0 <maqamylee0@gmail.com>
2024-01-07 22:16:07 -0800

    multiple writes to console

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+//script to print javascript is amazing
+
+const myVar = "C is fun"
+const myVar1 = "Python is cool"
+const myVar2 = "JavaScript is amazing"
+
+console.log(myVar);
+console.log(myVar1);
+console.log(myVar2);
