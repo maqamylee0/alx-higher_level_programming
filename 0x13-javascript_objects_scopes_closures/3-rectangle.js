e609abc4b49b902c09a45da9552b10c3072af003
maqamylee0 <maqamylee0@gmail.com>
2024-01-08 23:50:17 -0800

    print rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+//rectangle class
+
+class Rectangle{
+
+	constructor(w,h){
+		if (w > 1 && h > 1){
+			this.width = w;
+			this.height = h;
+		}
+	}
+	print(){
+		for (let i = 0; i < this.height; i++){
+			let row = "";
+			for (let j = 0; j < this.width; j++){
+				row += "X";
+			}
+			console.log(row);
+		}
+	}
+
+}
+module.exports = Rectangle;
