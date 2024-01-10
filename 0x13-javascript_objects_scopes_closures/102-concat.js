dfed7744a2df3460c9c9ef1a3b09848091ff38a1
maqamylee0 <maqamylee0@gmail.com>
2024-01-09 19:03:47 -0800

    fix concat 3

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
-//concat files
+// concat files
 
-//import fs
-const fs = require("fs");
+// import fs
+const fs = require('fs');
 
-function concatFiles(){
-	let content = "";
-	let file1 = fs.readFileSync(process.argv[2], 'utf8');
-	let file2 = fs.readFileSync(process.argv[3], 'utf8');
-	content += file1 + '\n' + file2;
-	fs.writeFileSync(process.argv[4], content, 'utf8')
+function concatFiles () {
+  let content = '';
+  const file1 = fs.readFileSync(process.argv[2], 'utf8');
+  const file2 = fs.readFileSync(process.argv[3], 'utf8');
+  content += file1 + file2;
+  fs.writeFileSync(process.argv[4], content, 'utf8');
 }
 concatFiles();
