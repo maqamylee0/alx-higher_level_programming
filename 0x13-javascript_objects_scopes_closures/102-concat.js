e513b726094279c59fd503da25c06841086934ca
maqamylee0 <maqamylee0@gmail.com>
2024-01-09 18:59:51 -0800

    fix concat

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -8,7 +8,7 @@ function concatFiles(){
 	let content = "";
 	let file1 = fs.readFileSync(process.argv[2], 'utf8');
 	let file2 = fs.readFileSync(process.argv[3], 'utf8');
-	let output = file1 + '\n' + file2;
-	fs.writeFileSync(process.argv[3], output, 'utf8')
+	content += file1 + '\n' + file2;
+	fs.writeFileSync(process.argv[3], content, 'utf8')
 }
 concatFiles();
