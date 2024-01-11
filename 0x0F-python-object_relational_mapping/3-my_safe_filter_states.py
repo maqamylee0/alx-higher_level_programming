009909db84220cb9ab3b4d2b3d7a4de9ad47ef91
maqamylee0 <maqamylee0@gmail.com>
2024-01-11 14:11:49 +0300

    secure parameterised query

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+try:
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC", (sys.argv[4],))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+except MySQLdb.Error as e:
+    print(f"Error {e}")
+finally:
+    cur.close()
+    conn.close()
