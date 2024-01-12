cdb83b6ffc18e47feb98216b429da6d6c7979b63
maqamylee0 <maqamylee0@gmail.com>
2024-01-12 10:38:10 +0300

    fetch first record using orm

---
+++ 8-model_state_fetch_first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Start link class to table in database and fetchall
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    first_state = session.query(State).order_by(State.id).first()
+    if first_state:
+        print("{}: {}".format(first_state.id, first_state.name))
+    else:
+        print("Nothing")
+
+    session.close()
