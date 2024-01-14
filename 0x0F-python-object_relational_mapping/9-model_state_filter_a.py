395a3d63d4519ed16661554a1b81c7ca72fa4732
maqamylee0 <maqamylee0@gmail.com>
2024-01-14 23:11:24 +0300

    check number of args

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -7,14 +7,15 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    if len(argv) == 4:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                               .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+        Session = sessionmaker(bind=engine)
+        session = Session()
 
-    states = session.query(State)\
-                    .filter(State.name.like('%a%'))\
-                    .order_by(State.id)
-    for i in states:
-        print(f"{state.id}: {state.name}")
-    session.close()
+        states = session.query(State)\
+                        .filter(State.name.like('%a%'))\
+                        .order_by(State.id)
+        for i in states:
+            print(f"{state.id}: {state.name}")
+        session.close()
