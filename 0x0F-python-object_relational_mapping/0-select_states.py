21050268aee72e71a798cb3b0009078d3bd4df9e
maqamylee0 <maqamylee0@gmail.com>
2024-01-14 23:32:16 +0300

    fix connection

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                                port=3306,
                                user=sys.argv[1],
                                passwd=sys.argv[2],
-                               db=sys.argv[3], charset="utf8")
+                               db=sys.argv[3])
         cur = conn.cursor()
         cur.execute("SELECT * FROM states ORDER BY id ASC")
         query_rows = cur.fetchall()
