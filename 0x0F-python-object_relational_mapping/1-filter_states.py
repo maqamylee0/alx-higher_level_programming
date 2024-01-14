0e7b1242a78908c2d675699ff4ffae5a8a580a41
maqamylee0 <maqamylee0@gmail.com>
2024-01-14 23:57:47 +0300

    fix indent

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,20 +4,23 @@ import MySQLdb
 """ Usage: ./1-filter_states.py <username> <password> <database> """
 
 if __name__ == '__main__':
-    try:
-        conn = MySQLdb.connect(host="localhost",
-                               port=3306,
-                               user=sys.argv[1],
-                               passwd=sys.argv[2],
-                               db=sys.argv[3], charset="utf8")
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM states WHERE \
-                    name LIKE 'N%' ORDER BY id ASC")
-        query_rows = cur.fetchall()
-        for row in query_rows:
-            print(row)
-    except MySQLdb.Error as e:
-        print(f"Error {e}")
-    finally:
-        cur.close()
-        conn.close()
+    if len(sys.argv) == 4:
+        try:
+            conn = MySQLdb.connect(host="localhost",
+                                   port=3306,
+                                   user=sys.argv[1],
+                                   passwd=sys.argv[2],
+                                   db=sys.argv[3], charset="utf8")
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM states WHERE \
+                        name LIKE 'N%' ORDER BY id ASC")
+            query_rows = cur.fetchall()
+            for row in query_rows:
+                print(row)
+        except MySQLdb.Error as e:
+            print(f"Error {e}")
+        finally:
+            cur.close()
+            conn.close()
+    else:
+        sys.exit(1)
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,21 +8,24 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    try:
-        conn = MySQLdb.connect(host="localhost",
-                               port=3306,
-                               user=sys.argv[1],
-                               passwd=sys.argv[2],
-                               db=sys.argv[3], charset="utf8")
-        cur = conn.cursor()
-        cur.execute(f"SELECT * FROM states \
-                    WHERE name LIKE '{sys.argv[4]}' \
-                    ORDER BY id ASC")
-        query_rows = cur.fetchall()
-        for row in query_rows:
-            print(row)
-    except MySQLdb.Error as e:
-        print(f"Error {e}")
-    finally:
-        cur.close()
-        conn.close()
+    if len(sys.argv == 5):
+        try:
+            conn = MySQLdb.connect(host="localhost",
+                                   port=3306,
+                                   user=sys.argv[1],
+                                   passwd=sys.argv[2],
+                                   db=sys.argv[3], charset="utf8")
+            cur = conn.cursor()
+            cur.execute(f"SELECT * FROM states \
+                        WHERE name LIKE '{sys.argv[4]}' \
+                        ORDER BY id ASC")
+            query_rows = cur.fetchall()
+            for row in query_rows:
+                print(row)
+        except MySQLdb.Error as e:
+            print(f"Error {e}")
+        finally:
+            cur.close()
+            conn.close()
+    else:
+        sys.exit(1)
