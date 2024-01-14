bae01c55aa04fddc6e51d7d9c5e2edce717a3d06
maqamylee0 <maqamylee0@gmail.com>
2024-01-14 23:00:07 +0300

    edit search method

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -7,13 +7,14 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
+    arg4 = sys.argv[4]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
 
     state = session.query(State)\
-                    .filter(State.name.like(f"{sys.argv[4]}"))\
+                    .filter(State.name.like(arg4))\
                     .order_by(State.id).first()
     if state:
         print(f"{state.id}")
