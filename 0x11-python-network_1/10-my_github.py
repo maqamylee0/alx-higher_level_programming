370f4fea0f3b41a310dd2fa0fa12d6d9354cd58b
maqamylee0 <maqamylee0@gmail.com>
2024-01-26 13:59:48 -0800

    get github id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+#using request library to send request to github api and show user id
+
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
+
+
+username = sys.argv[1]
+password = sys.argv[2]
+
+url = "https://api.github.com/user"
+if __name__ == "__main__":
+    auth_user = HTTPBasicAuth(username, password)
+    r = requests.get(url, auth=auth_user)
+    try:
+        response = r.json()
+        if response == {}:
+            print("None")
+        else:
+            print("{}".format(response.get("id")))
+    except ValueError:
+        print("Not a valid JSON")
