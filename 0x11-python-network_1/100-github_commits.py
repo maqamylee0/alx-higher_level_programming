1ace137a9e424d9aad4071a374f10d2803cf3cc3
maqamylee0 <maqamylee0@gmail.com>
2024-01-26 14:13:04 -0800

    get list of commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+#using request library to send request to github api and show last user commits
+
+import requests
+import sys
+
+
+repo = sys.argv[1]
+user = sys.argv[2]
+
+url = "https://api.github.com/repos/OWNER/REPO/commits"
+if __name__ == "__main__":
+    payload = {"owner": user,
+                "repo": repo
+                }
+    r = requests.get(url,  data=payload)
+    try:
+        response = r.json()
+        if response == []:
+            print("None")
+        else:
+            for i in response:
+                print("{} {}".format(response.get(i["sha"]), response.get(i["author_name"])))
+    except ValueError:
+        print("Not a valid JSON")
