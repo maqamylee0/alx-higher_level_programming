70c4d4ba9f688b67a7a26da1a4aaa5e6403a0540
maqamylee0 <maqamylee0@gmail.com>
2024-01-26 13:31:57 -0800

    handle erros codes with requests

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#using request library to send request and handle error
+
+import requests
+import sys
+
+url = sys.argv[1]
+
+if __name__ == "__main__":
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
