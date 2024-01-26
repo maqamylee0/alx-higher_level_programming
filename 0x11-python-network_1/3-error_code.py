20680a2d826cdc0c2e2a83e6b9af2b92b5fb2448
maqamylee0 <maqamylee0@gmail.com>
2024-01-26 13:09:45 -0800

    handle errors

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+#deal with errors
+
+import sys
+import urllib.request
+import urllib.error
+
+
+url = sys.argv[1]
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(url, data=data) as response:
+            data2 = response.read()
+            utf_8_content = data2.decode('utf-8')
+            print(utf_8_content)
+    except urllib.error.HTTError as e:
+        print("Error code: {}".format(e.code))
