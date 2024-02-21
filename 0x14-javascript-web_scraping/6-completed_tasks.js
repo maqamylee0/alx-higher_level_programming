1c964c0b45932da6611b7192a6b4fafbc6b26c57
maqamylee0 <maqamylee0@gmail.com>
2024-02-21 07:06:57 -0800

    completed tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/* script that computes the tasks completed id */
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, { json: true }, (err, response, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const tasksCompleted = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!tasksCompleted[todo.userId]) {
+        tasksCompleted[todo.userId] = 1;
+      } else {
+        tasksCompleted[todo.userId] += 1;
+      }
+    }
+  });
+  console.log(tasksCompleted);
+});
